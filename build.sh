2c7d2da94c6dea2d347f528b6cea176ec7d7af1c
Menithal <11814599+Menithal@users.noreply.github.com>
2025-10-13 18:20:31 +0300

    more pathfinding

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-#cd $(dirname "$0") #if someone using pyenv this behaves funny. just assume user is in root
-
 echo $(pwd)
-echo "Updating git submodules"
-git submodule update --init --recursive --remote
 
 echo "Building rust lib"
+
+rm libvpuppr/libvpuppr.gdextension .
 python libvpuppr/build.py --debug
 
 echo "Copying gdextension files"
